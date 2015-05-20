6a9258f406ecb5bf7cb275ec1fecb66b00f159e7
Ryan Hachtel <ryanhachtel@hotmail.com>
2015-05-20 10:19:29 -0700

    Initial Commit

---
+++ Western Sustainability/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Western Sustainability
+//
+//  Created by Ryan Hachtel on 5/20/15.
+//  Copyright (c) 2015 OOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Western Sustainability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Western Sustainability
+//
+//  Created by Ryan Hachtel on 5/20/15.
+//  Copyright (c) 2015 OOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
